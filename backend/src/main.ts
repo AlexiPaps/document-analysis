3be73a1e518fe0ab91ce6b10a976de9fa8d70b50
aalex <aalex@LAPTOP>
2025-10-01 15:29:38 +0200

    Final commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://document-analysis-eta.vercel.app',
-    ],
-    methods: 'GET,POST,PUT',
+    origin: 'https://document-analysis-eta.vercel.app',
+    methods: 'GET,POST,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
-    preflightContinue: false,
   });
 
-  await app.init();
+  await app.init(); // Use init() for serverless (Vercel)
 }
 bootstrap();
