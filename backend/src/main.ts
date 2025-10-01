7cb2386dd43151b6bb7d73fb41d3909d4dc6edf4
aalex <aalex@LAPTOP>
2025-10-01 15:08:04 +0200

    Final commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,20 @@
-// backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://document-analysis-eta.vercel.app'], // Allow frontend origin
-    methods: 'GET,POST', // Allow GraphQL methods
-    allowedHeaders: 'Content-Type, Authorization', // Allow necessary headers
+    origin: [
+      'http://localhost:5173',
+      'https://document-analysis-eta.vercel.app',
+    ],
+    methods: 'GET,POST,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+    preflightContinue: false,
   });
 
-  await app.listen(3000);
+  await app.init();
 }
 bootstrap();
